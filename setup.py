4860c4c09a4c72168e3a6a4b1e9445ea694fbf83
utapyngo <ut@pyngo.tom.ru>
2014-07-19 13:50:21 +0700

    Added support for fully qualified dot separated view names

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries',
         'Topic :: Utilities',
         'Environment :: Web Environment',
